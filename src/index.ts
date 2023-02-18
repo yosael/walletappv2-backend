f0620831abd16255b0bd2a0139339038d4fcc510
Edwin Gutierrez <yosael.gutierrez@gmail.com>
2023-02-17 19:05:06 -0600

    Add eslinter and prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (_, res) => {
-  return res.json({ message: "App setup" });
+app.get('/', (_, res) => {
+  return res.json({ message: 'App setup' });
 });
 
 app.listen(5000, () => {
-  console.log("Server started on port 5000");
+  console.log('Server started on port 5000');
 });
